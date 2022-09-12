bf006e99beb40a6c43bb74695a46b5e5795fa70b
tomas <tomaslaval22@gmail.com>
2022-09-11 23:28:42 -0300

    clase9
    a

---
+++ variable.js
@@ -0,0 +1,10 @@
+var pais="Francia";
+var continente="Europa";
+var antiguedad="1782";
+var pais_y_continente= pais +""+ continente;
+
+pais="Argentina";
+continente="Latinoamerica";
+
+console.log (pais, continente, antiguedad)
+alert (pais_y_continente)
